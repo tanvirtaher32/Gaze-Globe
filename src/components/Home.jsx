4f17e34b08df4a2c8ea638e5b4e6db7fbc13935f
tanvirtaher32 <tanvirtaher7@gmail.com>
2023-10-10 22:36:56 +0600

    home page done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,98 @@
 import React from 'react';
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.png';
+
+const headingOptions = {
+    pos: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%,-50%)',
+    textTransform: 'uppercase',
+    p: '4',
+    size: '4xl',
+  };
 
 const Home = () => {
+      
     return (
-        <div>
-            <h2>home</h2>
-        </div>
+        <Box>
+      <MyCarousel />
+
+      <Container maxW={'container.xl'} minH={'100vh'} p="16">
+        <Heading
+          textTransform={'uppercase'}
+          py="2"
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+          m="auto"
+        >
+          About Us
+        </Heading>
+
+        <Stack
+          h="full"
+          p={'4'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
+
+          <Text
+            letterSpacing={'widest'}
+            lineHeight={'190%'}
+            p={['4', '16']}
+            textAlign={'center'}
+          >
+            Gaze Globe is a streaming service that offers a vast library of movies, TV series, documentaries, and original content. It offers a curated selection of content from around the world, personalized recommendations, seamless streaming, exclusive Gaze Globe Originals, family-friendly options, offline viewing, and affordable subscriptions. The platform also provides a community of entertainment enthusiasts, allowing users to engage in discussions and connect with fellow viewers. Gaze Globe's advanced recommendation engine helps users find content tailored to their preferences, ensuring top-notch performance on both big screen TVs and mobile devices. The platform is a one-stop destination for family entertainment, offering a wide range of options for all.
+          </Text>
+        </Stack>
+      </Container>
+    </Box>
     );
 };
+
+const MyCarousel = () => (
+    <Carousel
+      autoPlay
+      infiniteLoop
+      interval={1000}
+      showStatus={false}
+      showThumbs={false}
+      showArrows={false}
+    >
+      <Box w="full" h={'100vh'}>
+        <Image src={img1} h="full" w={'full'} objectFit={'cover'} />
+        <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+          Watch The Future
+        </Heading>
+      </Box>
+      <Box w="full" h={'100vh'}>
+        <Image src={img2} h="full" w={'full'} objectFit={'cover'} />
+        <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
+          Future is Gaming
+        </Heading>
+      </Box>
+  
+      <Box w="full" h={'100vh'}>
+        <Image src={img3} h="full" w={'full'} objectFit={'cover'} />
+        <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+          Gaming on Console
+        </Heading>
+      </Box>
+  
+      <Box w="full" h={'100vh'}>
+        <Image src={img4} h="full" w={'full'} objectFit={'cover'} />
+        <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+          Night life is cool
+        </Heading>
+      </Box>
+    </Carousel>
+  );
 
 export default Home;
